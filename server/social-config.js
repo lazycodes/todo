479ab3ad792b0c2452eb0bdcb03c99b40d6f04dd
Chris Grimshaw-Jones <chrisgrimshawjones@gmail.com>
2015-08-29 19:28:46 +1200

    combined todo with social

---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1684770931757298',
+    secret: '9cdd96f2f6a0697fa19e1d49a3e86a28'
+});
